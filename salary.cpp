14da4ec4b8ff21450e841685ccd00543b55fc227
anushka-konwar123 <konwaranushkapahi@gmail.com>
2025-09-09 09:55:15 +0530

    Add files via upload

---
+++ salary.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main () {
+	double basic, hra, da, allowances;
+	cout << "Enter Basic Salary:";
+	cin>> basic;
+	cout<< "Enter HRA";
+	cin>>hra;
+	cout<<"Enter DA:";
+	cin >> da;
+	cout << "Enter other  Allowances:" ;
+	cin>> allowances;
+	 double totalsalary = basic + hra +da + allowances;
+	 cout << "Total salary = " << totalsalary << endl;
+	 return 0;
+}
