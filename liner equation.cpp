edf339f6f79faba13aab0a19e1a3d23c820c7b38
anushka-konwar123 <konwaranushkapahi@gmail.com>
2025-09-09 09:43:06 +0530

    Add files via upload

---
+++ liner equation.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main() {
+	double a1,b1,c1,a2,b2,c2;
+	cout<< "Enter coefficients of first equation (a1 b1 c1):";
+	cin>> a1>> b1>> c1;
+	cout << "Enter coefficients of second equation (a2 b2 c2):";
+	cin>> a2>> b2>> c2;
+	double det = a1* b2- a2*b1 ;
+	if ( det == 0) {
+		cout << " No unique solution exists ( lines and parallel or coincident )."<< endl;
+	} else {
+		double x = ( c1*b2- c2*b1)/ det;
+		double y= ( a1*c2- a2*c1)/ det;
+		cout << "Solution : x= " << x<<",y=" << y << endl;
+	}
+	return 0;
+}
