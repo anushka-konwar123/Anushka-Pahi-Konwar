3a8f326bc876efa0df5ed7bc6e5de0fab2ee038f
anushka-konwar123 <konwaranushkapahi@gmail.com>
2025-09-09 09:41:35 +0530

    Add files via upload

---
+++ positive integer.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main () {
+	int n;
+	cout<<"Enter a positive integer:";
+	cin>> n;
+	if (n<0) {
+		cout<<"Please enter a positive integer."<<endl;
+		return 1;
+	}
+	int sum =0;
+	int temp= n;
+	while ( temp != 0) {
+		sum += temp % 10;
+		temp/= 10;
+	}
+	cout<<"Sum of digits of" << n<< "=" << sum <<endl;
+	return 0;
+}
